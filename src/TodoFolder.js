781babf2cdbca82f1eefd64d383be00b2a273ec4
shenfeng1945 <823902826@qq.com>
2017-10-13 17:17:19 +0800

    新建todoFolder.js这个组件

---
+++ src/TodoFolder.js
@@ -0,0 +1,34 @@
+import React , {Component} from 'react'
+import $ from 'jquery'
+export default class TodoFolder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+
+        // this.folderIcon = (() => {
+        //     switch (this.props.todoFolderInfo.folderName) {
+        //         case '我的一天': return 'todo-folder-icon-day';
+        //         case '已加标记': return 'todo-folder-icon-flag';
+        //         default: return 'todo-folder-icon-default';
+        //     }
+        // })();
+        //
+        // this.folderModifyIcon = (() => {
+        //     switch (this.props.todoFolderInfo.folderName) {
+        //         case '我的一天':
+        //         case '已加标记':
+        //         default: return 'todo-folder-icon todo-folder-icon-modify';
+        //     }
+        // })();
+    }
+    render() {
+        return (
+            <div className="todoFolderItem">
+                <svg className="icon"><use xlinkHref="#icon-home"></use></svg>
+                <span className="todoFolderName">{this.props.todoFolderInfo.folderName}</span>
+                <span className="todoSum">{this.props.todoFolderInfo.todos.length}</span>
+            </div>
+        )
+
+    }
+}
