81ffde6bd61fa813dc599cce72a6f7a3e57a20bf
shenfeng1945 <823902826@qq.com>
2017-10-14 01:05:43 +0800

    创建todo的创建时间和更新时间

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,13 @@
 import React,{Component} from 'react'
+import moment from 'moment'
 export default class TodoItem extends Component {
     render(){
         return <div className="todoItem">
             <input type="checkbox" checked={this.props.todo.status==='completed'}
                    onChange={this.toggle.bind(this)}/>
             <span className="title">{this.props.todo.title}</span>
+            <span>{this.props.todo.status===''?moment(this.props.todo.createTime).format('YYYY/MM/DD HH:mm')
+                :moment(this.props.todo.updateTime).format('YYYY/MM/DD HH:mm')}</span>
             <button onClick={this.delete.bind(this)}>删除</button>
         </div>
     }
