8a2655593ce80faef95b6c97154f21b2266bbcae
shenfeng1945 <823902826@qq.com>
2017-10-13 17:19:41 +0800

    当点击‘创建清单‘时，弹出一个对话框，用于新建分组内容

---
+++ src/CreateDialog.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import $ from 'jquery';
+import './CreateDialog.css';
+
+export default class CreateDialog extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    keySubmit(e) {
+        if (e.key === 'Enter') {
+            this.props.onSubmit(e)
+        }
+    }
+    onSubmit(e) {
+        if (this.props.newFolder.trim() !== '') {
+            this.props.onSubmit(e)
+        }
+    }
+    render() {
+        return (
+            <div className="createFolder-Wrapper">
+                <div className="createFolder">
+                    <h3>新建分组</h3>
+                    <input type="text" placeholder="分组名称" value={this.props.newFolder}
+                           onChange={this.props.onChange} onKeyPress={this.keySubmit.bind(this)}/>
+                    <div className="createActions">
+                        <button onClick={this.props.onCancel}>取消</button>
+                        <button onClick={this.onSubmit.bind(this)}>新建</button>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
