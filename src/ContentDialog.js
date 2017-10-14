bd88acb28f22cb2a4b2f817ac3876574f4d4259d
shenfeng1945 <823902826@qq.com>
2017-10-14 12:59:15 +0800

    把editorDialog拼在上面

--- src/ContentDialog.js
+++ src/ContentDialog.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import $ from 'jquery';
 import CreateDialog from './CreateDialog'
 import {TodoModel} from "./leancloud"
+import EditorDialog from './EditorDialog'
+import {copyByJSON} from './copyByJSON'
 
 export default class ContentDialog extends Component {
     constructor(props) {
@@ -9,37 +11,58 @@ export default class ContentDialog extends Component {
         this.state = {
             curSelectTodoFolder: {},
             isActiveAddFolderDialog: false,
-            newFolder: ''
+            newFolder: '',
         }
-    }
 
+    }
     render() {
         return (
-            <CreateDialog onSubmit={this.addFolder.bind(this)}
-                          onCancel={this.cancelAddFolder.bind(this)}
-                          newFolder={this.state.newFolder}
-                          onChange={this.changeFolderTitle.bind(this)}/>
+            <div>
+                <CreateDialog onSubmit={this.addFolder.bind(this)}
+                              onCancel={this.cancelAddFolder.bind(this)}
+                              newFolder={this.state.newFolder}
+                              onChange={this.changeFolderTitle.bind(this)}/>
+
+                <EditorDialog onCancel={this.cancelEditorFolder.bind(this)}
+                              onSubmit={this.onEditorFolder.bind(this)}
+                              todoInfoFolderName={this.props.todoInfoFolderName}
+                              onChange={this.props.editorFolderName}
+                              deleteFolder={this.deleteFolder.bind(this)}/>
+            </div>
         )
     }
-    changeFolderTitle(e){
+    deleteFolder(e){
+        this.props.deleteFolder(e)
+        this.cancelEditorFolder()
+    }
+    onEditorFolder(e){
+        this.props.onEditorFolder(e)
+        this.cancelEditorFolder()
+    }
+    cancelEditorFolder(){
+        $('.editorFolder-Wrapper').removeClass('active')
+    }
+    changeFolderTitle(e) {
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.newFolder = e.target.value
         this.setState(stateCopy)
     }
-    addFolder(e){
-        let userId=this.props.userId
+
+    addFolder(e) {
+        let userId = this.props.userId
         let title = this.state.newFolder
-        TodoModel.createFolder(userId,title,(id)=>{
+        TodoModel.createFolder(userId, title, (id) => {
             let folder = {
-                usrId:id,
-                folderName:title
+                usrId: id,
+                folderName: title
             }
             this.props.onAddFolder(folder)
         })
 
         this.cancelAddFolder()
     }
-    cancelAddFolder(){
+
+    cancelAddFolder() {
         let stateCopy = JSON.parse(JSON.stringify(this.state))
         stateCopy.newFolder = ''
         this.setState(stateCopy)
