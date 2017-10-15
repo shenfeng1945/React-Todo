eca7fdb8196543067df0a57ebdccac0c2b2bc3bf
shenfeng1945 <823902826@qq.com>
2017-10-15 10:25:28 +0800

    无改动

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
+import './Weather.css'
 
 export default class Weather extends Component {
     render() {
         return (
             <ul className="weather">
-                <li>时间：{this.props.weather.currTime}</li>
-                <li>星期：{this.props.weather.week}</li>
-                <li>天气：{this.props.weather.currWeather}</li>
-                <li>城市：{this.props.weather.city}</li>
+                <li>时间：<span>{this.props.weather.currTime}</span></li>
+                <li>星期：<span>{this.props.weather.week}</span></li>
+                <li>天气：<span>{this.props.weather.currWeather}</span></li>
+                <li>城市：<span>{this.props.weather.city}</span></li>
             </ul>
         )
     }
