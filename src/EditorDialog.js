997d64f198f4b12bf21ed3f7b85e8b0153e88e1f
shenfeng1945 <823902826@qq.com>
2017-10-15 10:23:34 +0800

    button加className

--- src/EditorDialog.js
+++ src/EditorDialog.js
@@ -18,14 +18,14 @@ export default class EditorDialog extends Component {
     render() {
         return (
             <div className="editorFolder-Wrapper">
-                <div className="editorFolder">
-                    <h3>修改清单名称</h3>
+                <div className="editorFolder clearfix">
+                    <h3>修改分组名称</h3>
                     <input type="text"  value={this.props.todoInfoFolderName}
                            onChange={this.props.onChange} onKeyPress={this.keySubmit.bind(this)}/>
                     <div className="editorActions">
                         <svg className="icon" onClick={this.props.deleteFolder}><use xlinkHref="#icon-delete8e"></use></svg>
-                        <button onClick={this.props.onCancel}>取消</button>
-                        <button onClick={this.onSubmit.bind(this)}>确定</button>
+                        <button className="btn btn-cancel" onClick={this.props.onCancel}>取消</button>
+                        <button className="btn btn-enter" onClick={this.onSubmit.bind(this)}>确定</button>
                     </div>
                 </div>
             </div>
