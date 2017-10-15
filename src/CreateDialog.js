d735e9ead86d2eb673f00781ccb3bbb6b691bf83
shenfeng1945 <823902826@qq.com>
2017-10-15 10:22:56 +0800

    button加className

--- src/CreateDialog.js
+++ src/CreateDialog.js
@@ -20,13 +20,13 @@ export default class CreateDialog extends Component {
     render() {
         return (
             <div className="createFolder-Wrapper">
-                <div className="createFolder">
+                <div className="createFolder clearfix">
                     <h3>新建分组</h3>
                     <input type="text" placeholder="分组名称" value={this.props.newFolder}
                            onChange={this.props.onChange} onKeyPress={this.keySubmit.bind(this)}/>
                     <div className="createActions">
-                        <button onClick={this.props.onCancel}>取消</button>
-                        <button onClick={this.onSubmit.bind(this)}>新建</button>
+                        <button className="btn btn-cancel" onClick={this.props.onCancel}>取消</button>
+                        <button className="btn btn-enter" onClick={this.onSubmit.bind(this)}>新建</button>
                     </div>
                 </div>
             </div>
