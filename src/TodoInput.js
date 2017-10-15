5e73348cae6974fb13d00bfc70bb9f60d8671e56
shenfeng1945 <823902826@qq.com>
2017-10-15 10:25:04 +0800

    placeholder

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,18 +1,22 @@
-import React,{Component} from 'react'
-function submit(props,e){
-    if(e.key === 'Enter'){
-        if(e.target.value.trim()!== ''){
+import React, {Component} from 'react'
+
+function submit(props, e) {
+    if (e.key === 'Enter') {
+        if (e.target.value.trim() !== '') {
             props.onSubmit(e)
         }
     }
 }
-function changeTitle(props,e){
+
+function changeTitle(props, e) {
     props.onChange(e)
 }
-export default function(props){
+
+export default function (props) {
     return (
         <input type="text" className="todoInput" value={props.content}
-                   onKeyPress={submit.bind(null,props)}
-                   onChange={changeTitle.bind(null,props)}/>
+               placeholder="添加代办事项..."
+               onKeyPress={submit.bind(null, props)}
+               onChange={changeTitle.bind(null, props)}/>
     )
 }
